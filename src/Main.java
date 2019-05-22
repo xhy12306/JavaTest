da03943cbf00ca2d69ed23dc129a8af71ff24e03
xhy <906783915@qq.com>
2019-05-22 21:19:52 +0800

    第一次提交

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date);
+        String string = "abc";
+        System.out.println("Hello World!");
+        System.out.println("java Test");
+        System.out.println(string);
+    }
+}
